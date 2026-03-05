1e9c8ad8bac4158161ac11ef0ff8e3a5661c62dc
krishdugtal <krishdugtal2428@gmail.com>
2026-03-05 15:00:21 +0530

    uc10palindrome: CaseSensitive palindrome

---
+++ src/uc10palindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class uc10palindrome {
+
+    public static boolean isPalindrome(String str) {
+        String normalized = str.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Palindrome Checker App");
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
